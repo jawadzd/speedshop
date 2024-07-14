53851f12f37b6ca004b60b27bba288481b94b1e8
jawadzd <jawad.zein.deen@hotmail.com>
2024-07-14 20:21:42 +0300

    creating the card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { CardComponent } from './shared/components/card/card.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    CardComponent,
   ],
   imports: [
     BrowserModule,
