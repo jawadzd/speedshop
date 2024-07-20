af0d513e64b59e2db35fc57066ba1a8ac7d8a949
jawadzd <jawad.zein.deen@hotmail.com>
2024-07-20 14:04:18 +0300

    creating a temp footer for testing

--- src/app/features/features-routing.module.ts
+++ src/app/features/features-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProductListingComponent } from './product-listing/product-listing.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 const routes: Routes = [
-  { path: '', component: ProductListingComponent }
+  { path: '', component: LandingPageComponent }
 ];
 
 @NgModule({
