24c00a2434fc630b9a962c0d4681a8838b72d8b6
jawadzd <jawad.zein.deen@hotmail.com>
2024-07-21 15:46:01 +0300

    add the authentication guard to the signup page too

--- src/app/core/app-shell/shell-routing.module.ts
+++ src/app/core/app-shell/shell-routing.module.ts
@@ -3,14 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { FeatureComponent } from '../../features/feature.component';
 import { LoginComponent } from '../auth/login/login.component';
 import { SignupComponent } from '../auth/signup/signup.component';
-import { AuthGuard } from '../auth/guards/auth.guard';
+import { authGuard } from '../auth/guards/auth.guard';
 
 
 const routes: Routes = [
   { path: '', redirectTo: 'feature', pathMatch: 'full' },
   { path: 'feature', component: FeatureComponent, loadChildren: () => import('../../features/features.module').then(m => m.FeaturesModule) },
-  { path: 'login', component: LoginComponent  , canActivate: [AuthGuard]},
-  {path:'signup',component:SignupComponent}
+  { path: 'login', component: LoginComponent  , canActivate: [authGuard]},
+  {path:'signup',component:SignupComponent, canActivate: [authGuard]}
 ];
 
 @NgModule({
--- src/app/core/auth/guards/auth.guard.ts
+++ src/app/core/auth/guards/auth.guard.ts
@@ -1,11 +1,12 @@
+import { CanActivate, CanActivateFn } from '@angular/router';
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import{ Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class authGuard implements CanActivate {
 
   constructor(private authService: AuthService, private router: Router) {}
 
