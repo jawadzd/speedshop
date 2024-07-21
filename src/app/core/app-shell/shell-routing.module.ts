b4c93dfce57417b216d0d3de8297125634c623ab
jawadzd <jawad.zein.deen@hotmail.com>
2024-07-21 14:17:49 +0300

    create an auth guard that does not let you go to login page if already logged in

--- src/app/core/app-shell/shell-routing.module.ts
+++ src/app/core/app-shell/shell-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { FeatureComponent } from '../../features/feature.component';
 import { LoginComponent } from '../auth/login/login.component';
 import { SignupComponent } from '../auth/signup/signup.component';
+import { AuthGuard } from '../auth/guards/auth.guard';
+
 
 const routes: Routes = [
   { path: '', redirectTo: 'feature', pathMatch: 'full' },
   { path: 'feature', component: FeatureComponent, loadChildren: () => import('../../features/features.module').then(m => m.FeaturesModule) },
-  { path: 'login', component: LoginComponent },
+  { path: 'login', component: LoginComponent  , canActivate: [AuthGuard]},
   {path:'signup',component:SignupComponent}
 ];
 
