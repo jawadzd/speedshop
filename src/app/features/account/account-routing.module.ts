9f3e5b8d12e5532e855767bb1a2bf45e86c9ad7e
jawadzd <jawad.zein.deen@hotmail.com>
2024-07-23 23:27:25 +0300

    adding the data component and fixing the routing inside

--- src/app/features/account/account-routing.module.ts
+++ src/app/features/account/account-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccountComponent } from './account.component';
+import { DataComponent } from './components/data/data.component';
 
 const routes: Routes = [
   { path: '', component: AccountComponent },
+  {path: 'account/data',component: DataComponent}
 
 ];
 
--- src/app/features/account/account.module.ts
+++ src/app/features/account/account.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AccountRoutingModule } from './account-routing.module';
 import { AccountComponent } from './account.component';
+import { DataComponent } from './components/data/data.component';
 
 
 @NgModule({
   declarations: [
-    AccountComponent
+    AccountComponent,
+    DataComponent
   ],
   imports: [
     CommonModule,
