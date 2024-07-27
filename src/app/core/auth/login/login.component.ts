2a9021fa6b34a39e0e56a94f564d1e22384ba881
jawadzd <jawad.zein.deen@hotmail.com>
2024-07-27 17:07:56 +0300

    doing the unsubscribe commands after the login is done

--- src/app/core/auth/login/login.component.ts
+++ src/app/core/auth/login/login.component.ts
@@ -1,16 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Router } from '@angular/router';
 import { login } from './store/auth.actions';
 import { AuthState } from './store/auth.reducer';
 import { ILoginRequest } from './models/login-request.model';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnDestroy {
+
+
+  private unsubscribe$ = new Subject<void>();
+
   username: string = '';
   password: string = '';
 
@@ -27,10 +34,16 @@ export class LoginComponent {
     this.store.select(state => state.auth).subscribe(authState => {
       if (authState.user && authState.user.Login && authState.user.Login.AccessToken) {
         alert('Login successful');
-        this.router.navigate(['/shell/feature/account']); // Redirect to home page or another route
+        this.router.navigate(['/shell/feature/account']);
       } else if (authState.error) {
         alert('Login failed. Please check your username and password.');
       }
     });
+  }
+
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 }
