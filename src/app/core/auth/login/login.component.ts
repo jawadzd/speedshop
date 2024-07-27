bd3f5a340fcc129c637f0234267fbfa6ac0e128a
jawadzd <jawad.zein.deen@hotmail.com>
2024-07-27 17:41:00 +0300

    fixed the login issue showing the alert multiple times

--- src/app/core/auth/login/login.component.ts
+++ src/app/core/auth/login/login.component.ts
@@ -1,26 +1,37 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Router } from '@angular/router';
 import { login } from './store/auth.actions';
 import { IAuthState } from './store/auth.reducer';
 import { ILoginRequest } from './models/login-request.model';
 import { Subject } from 'rxjs';
-
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnDestroy {
-
-
+export class LoginComponent implements OnInit, OnDestroy {
   private unsubscribe$ = new Subject<void>();
 
   username: string = '';
   password: string = '';
 
-  constructor(private store: Store<{ auth: IAuthState }>, private router: Router) { }
+  constructor(private store: Store<{ auth: IAuthState }>, private router: Router) {}
+
+  ngOnInit(): void {
+    this.store.select(state => state.auth)
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(authState => {
+        if (authState.user && authState.user.Login && authState.user.Login.AccessToken) {
+          alert('Login successful');
+          this.router.navigate(['/shell/feature/account']);
+        } else if (authState.error) {
+          alert('Login failed. Please check your username and password.');
+        }
+      });
+  }
 
   onSubmit(): void {
     const request: ILoginRequest = {
@@ -29,17 +40,7 @@ export class LoginComponent implements OnDestroy {
     };
 
     this.store.dispatch(login({ request: request }));
-
-    this.store.select(state => state.auth).subscribe(authState => {
-      if (authState.user && authState.user.Login && authState.user.Login.AccessToken) {
-        alert('Login successful');
-        this.router.navigate(['/shell/feature/account']);
-      } else if (authState.error) {
-        alert('Login failed. Please check your username and password.');
-      }
-    });
   }
-
 
   ngOnDestroy(): void {
     this.unsubscribe$.next();
