735f3c40732afc875ad0f3f02055b1b6b133ce78
jawadzd <jawad.zein.deen@hotmail.com>
2024-08-03 17:15:58 +0300

    fixing the routing and the auth guard for profile

--- src/app/features/account/account-routing.module.ts
+++ src/app/features/account/account-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AccountComponent } from './account.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { authGuard } from '../../core/auth/guards/auth.guard';
 
 const routes: Routes = [
-  {path : '', component: ProfileComponent},
+  {path : '', redirectTo: 'profile', pathMatch: 'full'},
+  {path : 'profile', canActivate: [authGuard],component: ProfileComponent},
 
 ];
 
