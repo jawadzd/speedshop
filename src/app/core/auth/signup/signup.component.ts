11529123b16893690aefa0a3e57b774644b6822c
jawadzd <jawad.zein.deen@hotmail.com>
2024-08-06 14:58:14 +0300

    fixing the signup component

--- src/app/core/auth/signup/signup.component.ts
+++ src/app/core/auth/signup/signup.component.ts
@@ -1,36 +1,45 @@
-import { Component, OnDestroy } from '@angular/core';
-import { select, Store } from '@ngrx/store';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Store, select } from '@ngrx/store';
 import { Router } from '@angular/router';
 import { signup } from './store/signup.actions';
 import { ISignupState } from './store/signup.reducer';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
-import { selectSignupUser ,selectSignupError} from './store/signup.selectors';
+import { selectSignupUser, selectSignupError } from './store/signup.selectors';
+import Swal from 'sweetalert2';
+import { AbstractControl, ValidatorFn } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
-export class SignupComponent implements OnDestroy {
+export class SignupComponent implements OnInit, OnDestroy {
   private unsubscribe$ = new Subject<void>();
+
   firstname: string = '';
   lastname: string = '';
   email: string = '';
   password: string = '';
 
-
   user$ = this.store.pipe(select(selectSignupUser));
   error$ = this.store.pipe(select(selectSignupError));
 
+  constructor(private store: Store<ISignupState>, private router: Router) {}
 
-  constructor(private store: Store<ISignupState>, private router: Router) {
+  ngOnInit(): void {
     this.user$
       .pipe(takeUntil(this.unsubscribe$))
       .subscribe(user => {
         if (user && user.id) {
-          alert('Signup successful, please login to continue');
-          this.router.navigate(['/shell/login']);
+          Swal.fire({
+            icon: 'success',
+            title: 'Signup Successful',
+            text: 'Please login to continue',
+            confirmButtonText: 'Continue'
+          }).then(() => {
+            this.router.navigate(['/shell/login']);
+          });
         }
       });
 
@@ -38,11 +47,24 @@ export class SignupComponent implements OnDestroy {
       .pipe(takeUntil(this.unsubscribe$))
       .subscribe(error => {
         if (error) {
-          alert('Signup failed. Please try again.');
+          Swal.fire({
+            icon: 'error',
+            title: 'Signup Failed',
+            text: 'Please try again.',
+            confirmButtonText: 'Try Again'
+          });
         }
       });
   }
-
+  emailDomainValidator(): ValidatorFn {
+    return (control: AbstractControl): {[key: string]: any} | null => {
+      const email = control.value;
+      if (email && !email.includes('@') || !email.endsWith('.com')) {
+        return { 'invalidEmail': true };
+      }
+      return null;
+    };
+  }
 
   onSubmit(): void {
     const request = {
