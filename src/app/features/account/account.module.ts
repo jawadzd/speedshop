338d7bc853b8cff843749400d4565afabdafdd01
jawadzd <jawad.zein.deen@hotmail.com>
2024-08-11 12:24:47 +0300

    generating the cart component

--- src/app/features/account/account.module.ts
+++ src/app/features/account/account.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AccountRoutingModule } from './account-routing.module';
 import { AccountComponent } from './account.component';
+import { CartComponent } from './components/cart/cart.component';
 
 
 @NgModule({
   declarations: [
-    AccountComponent
+    AccountComponent,
+    CartComponent
   ],
   imports: [
     CommonModule,
