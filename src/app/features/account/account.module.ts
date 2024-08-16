e6836eafb9154fea435b47b3178eecec777018a8
jawadzd <jawad.zein.deen@hotmail.com>
2024-08-16 17:34:04 +0300

    fixing the scss and transaltion in the cart

--- src/app/features/account/account.module.ts
+++ src/app/features/account/account.module.ts
@@ -6,15 +6,16 @@ import { CartComponent } from './components/cart/cart.component';
 import { AgGridModule } from 'ag-grid-angular'; //this is the module that will be used to display the cart items in a grid
 import { DeleteComponent } from '../../shared/components/grid_components/delete/delete.component';
 import { FormsModule } from '@angular/forms';
+import { TranslateService } from '@ngx-translate/core';
+import { TranslateModule } from '@ngx-translate/core';
 //this is the 4th level module that handle the account components whicha re special for each user
 
 @NgModule({
   declarations: [
     AccountComponent,
     CartComponent,
-
     DeleteComponent,
   ],
-  imports: [CommonModule, AccountRoutingModule, AgGridModule,FormsModule],
+  imports: [CommonModule, AccountRoutingModule, AgGridModule,FormsModule,TranslateModule],
 })
 export class AccountModule {}
--- src/app/features/account/components/cart/cart.component.ts
+++ src/app/features/account/components/cart/cart.component.ts
@@ -2,9 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { CartService } from '../../services/cart.service';
 import { AuthService } from '../../../../core/auth/services/auth.service';
 import { ProductService } from '../../services/single-item.service';
-import { Observable, forkJoin, switchMap, map ,catchError,of } from 'rxjs';
+import { Observable, forkJoin, switchMap, map, catchError, of } from 'rxjs';
 import { IItem } from '../../../../shared/models/item.model';
-import { ColDef} from 'ag-grid-community';
+import { ColDef } from 'ag-grid-community';
+import { TranslateService } from '@ngx-translate/core';
+import Swal from 'sweetalert2';
 import { DeleteComponent } from '../../../../shared/components/grid_components/delete/delete.component';
 
 @Component({
@@ -15,31 +17,30 @@ import { DeleteComponent } from '../../../../shared/components/grid_components/d
 export class CartComponent implements OnInit {
   context = { componentParent: this };
 
-  rowData: any[]=[];
-
-  columnDefs: ColDef[] = [
-    { headerName: 'ID', field: 'id', width: 70 },
-    { headerName: 'Title', field: 'title', width: 250 },
-    { headerName: 'Price', field: 'price', width: 100 },
-    { headerName: 'Quantity', field: 'quantity', width: 100 },
-    { headerName: 'Image', field: 'image', width: 150, cellRenderer: this.imageRenderer },
-    { headerName: "Actions", cellRenderer: 'deleteButtonRenderer' } 
-  ];
+  rowData$: Observable<{ id: number; title: string; price: number; quantity: number; image: string }[]> | undefined;
+  total$: Observable<number> | undefined;
+  columnDefs: ColDef[] = [];
 
   frameworkComponents: any = {
     deleteButtonRenderer: DeleteComponent
-
   };
-
-  rowData$: Observable<{ id: number; title: string; price: number; quantity: number; image: string }[]> | undefined;
-  total$: Observable<number> | undefined;
 
   constructor(
     private cartService: CartService,
     private authService: AuthService,
-    private productService: ProductService
+    private productService: ProductService,
+    private translate: TranslateService
   ) {}
+
   ngOnInit(): void {
+    this.updateColumnDefs(); // Call this to set column definitions
+    this.loadCartData();
+    this.translate.onLangChange.subscribe(() => {
+      this.updateColumnDefs(); // Update column definitions on language change
+    });
+  }
+
+  private loadCartData(): void {
     const userId = this.authService.getUserId();
     if (userId !== null) {
       this.rowData$ = this.cartService.getUserCart(userId).pipe(
@@ -65,7 +66,7 @@ export class CartComponent implements OnInit {
           return of([]); // Return an empty array in case of error
         })
       );
-  
+
       this.total$ = this.rowData$.pipe(
         map(items => items.reduce((total, item) => total + item.price * item.quantity, 0)),
         catchError(error => {
@@ -78,72 +79,64 @@ export class CartComponent implements OnInit {
       this.rowData$ = of([]); // Ensure rowData$ is set to an empty array if user ID is not found
     }
   }
-  
-// Method to delete a row by product ID
-deleteRow(rowIndex: number): void {
-  if (this.rowData$) {
-    this.rowData$.subscribe(rowData => {
-      const productId = rowData[rowIndex]?.id; // Use optional chaining to avoid errors if rowData[rowIndex] is undefined
+
+  private updateColumnDefs(): void {
+    this.translate.get([
+      'ID',
+      'TITLE',
+      'PRICE',
+      'QUANTITY',
+      'IMAGE',
+      'ACTIONS'
+    ]).subscribe(translations => {
+      this.columnDefs = [
+        { headerName: translations['ID'], field: 'id', width: 70 },
+        { headerName: translations['TITLE'], field: 'title', width: 250 },
+        { headerName: translations['PRICE'], field: 'price', width: 100 },
+        { headerName: translations['QUANTITY'], field: 'quantity', width: 100 },
+        { headerName: translations['IMAGE'], field: 'image', width: 150, cellRenderer: this.imageRenderer },
+        { headerName: translations['ACTIONS'], cellRenderer: 'deleteButtonRenderer' }
+      ];
+    });
+  }
+
+  deleteRow(rowIndex: number): void {
+    this.rowData$?.subscribe(rowData => {
+      const productId = rowData[rowIndex]?.id;
       if (productId !== undefined) {
-        this.onDelete(productId);
+        Swal.fire({
+          title: 'Are you sure?',
+          text: 'Do you really want to delete this item from your cart?',
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonText: 'Yes, delete it!',
+          cancelButtonText: 'No, keep it'
+        }).then((result) => {
+          if (result.isConfirmed) {
+            this.onDelete(productId);
+          }
+        });
       } else {
         console.error('Product ID is undefined at rowIndex:', rowIndex);
       }
     }, error => {
       console.error('Error subscribing to rowData$:', error);
     });
-  } else {
-    console.error('rowData$ is undefined');
   }
-}
 
-onDelete(productId: number): void {
-  const userId = this.authService.getUserId();
-  if (userId !== null) {
-    this.cartService.removeItemFromCart(userId, productId).subscribe(() => {
-      // Refresh the cart data after deletion
-      this.rowData$ = this.cartService.getUserCart(userId).pipe(
-        switchMap(cartItems => {
-          const productRequests = cartItems.map(item =>
-            this.productService.getProductDetailsById(item.productId).pipe(
-              map((product: IItem) => ({
-                id: product.id,
-                title: product.title,
-                price: product.price,
-                quantity: item.quantity,
-                image: product.image
-              }))
-            )
-          );
-          return forkJoin(productRequests);
-        }),
-        catchError(error => {
-          console.error('Error fetching updated cart items:', error);
-          return of([]); // Return an empty array in case of error
-        })
-      );
-      this.updateTotal();
-    }, error => {
-      console.error('Error removing item from cart:', error);
-    });
+  private onDelete(productId: number): void {
+    const userId = this.authService.getUserId();
+    if (userId !== null) {
+      this.cartService.removeItemFromCart(userId, productId).subscribe(() => {
+        // Refresh the cart data after deletion
+        this.loadCartData(); // Call the method to reload cart data
+      }, error => {
+        console.error('Error removing item from cart:', error);
+      });
+    }
   }
-}
 
-private updateTotal(): void {
-  if (this.rowData$) {
-    this.total$ = this.rowData$.pipe(
-      map(items => items.reduce((total, item) => total + item.price * item.quantity, 0)),
-      catchError(error => {
-        console.error('Error updating total price:', error);
-        return of(0); // Return 0 in case of error
-      })
-    );
-  } else {
-    console.error('rowData$ is undefined');
+  private imageRenderer(params: { value: string }): string {
+    return `<img src="${params.value}" width="100" height="100" style="object-fit: contain;">`;
   }
-}
-
-imageRenderer(params: { value: string }): string {
-  return `<img src="${params.value}" width="100" height="100" style="object-fit: contain;">`;
-}
 }
