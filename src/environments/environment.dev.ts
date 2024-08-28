63719bd15f54c83bf25d7c0feacb59749dd2f910
Jawad Zeyneddin <60114992+jawadzd@users.noreply.github.com>
2024-08-28 15:51:24 +0300

    Update environment.dev.ts

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,5 +1,5 @@
 export const environment = {
     authenticationApi: 'http://173.249.40.235:5005/api/',
-    productsApi:'https://fakestoreapi.com/'
+    productsApi:'https://fakestoreapi.com/',
    apiKeyGeo: process.env.API_KEY_GEO || 'default-key' 
 }
